191c74a6b59207182bb3cd8580d91213a7f04d2f
schismtv <andteds1981@gmail.com>
2016-04-25 14:04:01 +0100

    update

--- plugin.video.schismwizard/downloader.py
+++ plugin.video.schismwizard/downloader.py
@@ -1,38 +1,34 @@
 import xbmcgui
 import urllib
-
-
-class aresdownload(urllib.FancyURLopener):
-    version = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
-
-
+import time
 
 def download(url, dest, dp = None):
-
-	if url.find('ares1') == -1:
-
-		if not dp:
-			dp = xbmcgui.DialogProgress()
-			dp.create("Please Wait..","Downloading & Copying File",' ', ' ')
-		dp.update(0)
-		urllib.urlretrieve(url,dest,lambda nb, bs, fs, url=url: _pbhook(nb,bs,fs,url,dp))
-		
-	else:
-	
-		if not dp:
-			dp = xbmcgui.DialogProgress()
-			dp.create("Please Wait..","Downloading & Copying File",' ', ' ')
-		dp.update(0)
-		aresdownload().retrieve(url,dest,lambda nb, bs, fs, url=url: _pbhook(nb,bs,fs,url,dp))
-		
- 
-def _pbhook(numblocks, blocksize, filesize, url, dp):
-    try:
-        percent = min((numblocks*blocksize*100)/filesize, 100)
-        dp.update(percent)
-    except:
-        percent = 100
-        dp.update(percent)
-    if dp.iscanceled(): 
-        raise Exception("Canceled")
-        dp.close()
+    if not dp:
+        dp = xbmcgui.DialogProgress()
+        dp.create("","",' ', ' ')
+    dp.update(0)
+    start_time=time.time()
+    urllib.urlretrieve(url, dest, lambda nb, bs, fs: _pbhook(nb, bs, fs, dp, start_time))
+     
+def _pbhook(numblocks, blocksize, filesize, dp, start_time):
+        try: 
+            percent = min(numblocks * blocksize * 100 / filesize, 100)
+            currently_downloaded = float(numblocks) * blocksize / (1024 * 1024) 
+            kbps_speed = numblocks * blocksize / (time.time() - start_time) 
+            if kbps_speed > 0: 
+                eta = (filesize - numblocks * blocksize) / kbps_speed 
+            else: 
+                eta = 0 
+            kbps_speed = kbps_speed / 1024 
+            total = float(filesize) / (1024 * 1024) 
+            mbs = '%.02f MB of %.02f MB' % (currently_downloaded, total) 
+            e = 'Speed: %.02f Kb/s ' % kbps_speed 
+            e += 'ETA: %02d:%02d' % divmod(eta, 60)
+            string = '[COLOR Lime]Downloading... Please Wait...[/COLOR]'
+            dp.update(percent, mbs, e,string)
+        except: 
+            percent = 100 
+            dp.update(percent) 
+        if dp.iscanceled(): 
+			raise Exception("Canceled")
+			dp.close()
--- plugin.video.schismwizard/service.py
+++ plugin.video.schismwizard/service.py
@@ -1,5 +1,4 @@
 import xbmc, xbmcaddon, xbmcgui, xbmcplugin,os,base64,sys,xbmcvfs
-from addon.common.addon import Addon
 import urllib2,urllib
 import extract
 import downloader
